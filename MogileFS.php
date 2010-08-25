a71946e55650ee8630469e405e1cdb5ff8dc5776
ash2k <ash2kk@gmail.com>
2010-08-25 19:22:02 +0600

    Code cleanup: double to single quotes, underline for protected members, 'self' instead of 'MogileFS' class name, '_debug' is now boolean.

--- MogileFS.php
+++ MogileFS.php
@@ -1,6 +1,6 @@
 <?php
 
-/* MogileFS.class.php - Class for accessing the Mogile File System
+/* MogileFS.php - Class for accessing the Mogile File System
  * Copyright (C) 2007 Interactive Path, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -38,14 +38,14 @@ class MogileFS {
     const ERROR = 'ERR';          // Tracker error code
     const DEFAULT_PORT = 7001;    // Tracker port
 
-    protected $domain;
-    protected $class;
-    protected $trackers;
-    protected $socket;
-    protected $connectTimeout;
-    protected $putTimeout;
-    protected $getTimeout;
-    protected $debug;
+    protected $_domain;
+    protected $_class;
+    protected $_trackers;
+    protected $_socket;
+    protected $_connectTimeout;
+    protected $_putTimeout;
+    protected $_getTimeout;
+    protected $_debug;
 
     public function __construct($domain, $class, $trackers) {
         $this->setDomain($domain);
@@ -54,113 +54,113 @@ class MogileFS {
         $this->setConnectTimeout(3);
         $this->setPutTimeout(10);
         $this->setGetTimeout(10);
-        $this->setDebug(0);
+        $this->setDebug(false);
     }
 
     public function getDebug() {
-        return $this->debug;
+        return $this->_debug;
     }
 
-    public function setDebug($level) {
-        return $this->debug = $level;
+    public function setDebug($debug) {
+        return $this->_debug = (bool) $debug;
     }
 
     public function getConnectTimeout() {
-        return $this->connectTimeout;
+        return $this->_connectTimeout;
     }
 
     public function setConnectTimeout($timeout) {
         if ($timeout > 0)
-            return $this->connectTimeout = $timeout;
+            return $this->_connectTimeout = $timeout;
         else
-            throw new Exception(get_class($this) . "::setConnectTimeout expects a positive integer");
+            throw new Exception(get_class($this) . '::setConnectTimeout expects a positive integer');
     }
 
     public function getPutTimeout() {
-        return $this->putTimeout;
+        return $this->_putTimeout;
     }
 
     public function setPutTimeout($timeout) {
         if ($timeout > 0)
-            return $this->putTimeout = $timeout;
+            return $this->_putTimeout = $timeout;
         else
-            throw new Exception(get_class($this) . "::setPutTimeout expects a positive integer");
+            throw new Exception(get_class($this) . '::setPutTimeout expects a positive integer');
     }
 
     public function getGetTimeout() {
-        return $this->getTimeout;
+        return $this->_getTimeout;
     }
 
     public function setGetTimeout($timeout) {
         if ($timeout > 0)
-            return $this->getTimeout = $timeout;
+            return $this->_getTimeout = $timeout;
         else
-            throw new Exception(get_class($this) . "::setGetTimeout expects a positive integer");
+            throw new Exception(get_class($this) . '::setGetTimeout expects a positive integer');
     }
 
     public function getHosts() {
-        return $this->trackers;
+        return $this->_trackers;
     }
 
     public function setHosts($trackers) {
         if (is_scalar($trackers))
-            $this->trackers = Array($trackers);
+            $this->_trackers = Array($trackers);
         elseif (is_array($trackers))
-            $this->trackers = $trackers;
+            $this->_trackers = $trackers;
         else
-            throw new Exception(get_class($this) . "::setHosts unrecognized host argument");
+            throw new Exception(get_class($this) . '::setHosts unrecognized trackers argument');
     }
 
     public function getDomain() {
-        return $this->domain;
+        return $this->_domain;
     }
 
     public function setDomain($domain) {
         if (is_scalar($domain))
-            return $this->domain = $domain;
+            return $this->_domain = $domain;
         else
-            throw new Exception(get_class($this) . "::setDomain unrecognized domain argument");
+            throw new Exception(get_class($this) . '::setDomain unrecognized domain argument');
     }
 
     public function getClass() {
-        return $this->class;
+        return $this->_class;
     }
 
     public function setClass($class) {
         if (is_scalar($class))
-            return $this->class = $class;
+            return $this->_class = $class;
         else
-            throw new Exception(get_class($this) . "::setClass unrecognized class argument");
+            throw new Exception(get_class($this) . '::setClass unrecognized class argument');
     }
 
     // Connect to a mogilefsd; scans through the list of daemons and tries to connect one.
     public function getConnection() {
-        if ($this->socket && is_resource($this->socket) && !feof($this->socket))
-            return $this->socket;
+        if ($this->_socket && is_resource($this->_socket) && !feof($this->_socket))
+            return $this->_socket;
 
-        foreach ($this->trackers as $host) {
+        foreach ($this->_trackers as $host) {
             $parts = parse_url($host);
             if (!isset($parts['port']))
-                $parts['port'] = MogileFS::DEFAULT_PORT;
+                $parts['port'] = self::DEFAULT_PORT;
 
             $errno = null;
             $errstr = null;
-            $this->socket = fsockopen($parts['host'], $parts['port'], $errno, $errstr, $this->connectTimeout);
-            if ($this->socket)
+            $this->_socket = fsockopen($parts['host'], $parts['port'], $errno, $errstr, $this->_connectTimeout);
+            if ($this->_socket)
                 break;
         }
 
-        if (!is_resource($this->socket) || feof($this->socket))
-            throw new Exception(get_class($this) . "::doConnection failed to obtain connection");
+        if (!is_resource($this->_socket) || feof($this->_socket))
+            throw new Exception(get_class($this) . '::getConnection failed to obtain connection');
         else
-            return $this->socket;
+            return $this->_socket;
     }
 
     // Send a request to mogilefsd and parse the result.
     protected function doRequest($cmd, $args = Array()) {
         try {
-            $args['domain'] = $this->domain;
-            $args['class'] = $this->class;
+            $args['domain'] = $this->_domain;
+            $args['class'] = $this->_class;
             $params = '';
             foreach ($args as $key => $value)
                 $params .= '&' . urlencode($key) . '=' . urlencode($value);
@@ -169,14 +169,14 @@ class MogileFS {
 
             $result = fwrite($socket, $cmd . $params . "\n");
             if ($result === false)
-                throw new Exception(get_class($this) . "::doRequest write failed");
+                throw new Exception(get_class($this) . '::doRequest write failed');
             $line = fgets($socket);
             if ($line === false)
-                throw new Exception(get_class($this) . "::doRequest read failed");
+                throw new Exception(get_class($this) . '::doRequest read failed');
 
             //print "[$line]\n";
             $words = explode(' ', $line);
-            if ($words[0] == MogileFS::SUCCESS)
+            if ($words[0] == self::SUCCESS)
                 parse_str(trim($words[1]), $result);
             else {
                 if (!isset($words[1]))
@@ -189,7 +189,7 @@ class MogileFS {
                         throw new Exception(get_class($this) . "::doRequest empty_file {$args['key']}");
 
                     default:
-                        throw new Exception(get_class($this) . "::doRequest " . trim(urldecode($line)));
+                        throw new Exception(get_class($this) . '::doRequest ' . trim(urldecode($line)));
                 }
             }
             return $result;
@@ -204,7 +204,7 @@ class MogileFS {
 
     // Return a list of domains
     public function getDomains() {
-        $res = $this->doRequest(MogileFS::GET_DOMAINS);
+        $res = $this->doRequest(self::GET_DOMAINS);
 
         $domains = Array();
         for ($i = 1; $i <= $res['domains']; $i++) {
@@ -219,10 +219,10 @@ class MogileFS {
 
     public function exists($key) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::exists key cannot be null");
+            throw new Exception(get_class($this) . '::exists key cannot be null');
 
         try {
-            $this->doRequest(MogileFS::GET_PATHS, Array('key' => $key));
+            $this->doRequest(self::GET_PATHS, Array('key' => $key));
             return true;
         } catch (Exception $e) {
             return false;
@@ -232,9 +232,9 @@ class MogileFS {
     // Get an array of paths
     public function getPaths($key) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::getPaths key cannot be null");
+            throw new Exception(get_class($this) . '::getPaths key cannot be null');
 
-        $result = $this->doRequest(MogileFS::GET_PATHS, Array('key' => $key));
+        $result = $this->doRequest(self::GET_PATHS, Array('key' => $key));
         unset($result['paths']);
         return $result;
     }
@@ -242,25 +242,29 @@ class MogileFS {
     // Delete a file from system
     public function delete($key) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::delete key cannot be null");
-        $this->doRequest(MogileFS::DELETE, Array('key' => $key));
+            throw new Exception(get_class($this) . '::delete key cannot be null');
+        $this->doRequest(self::DELETE, Array('key' => $key));
         return true;
     }
 
     // Rename a file
     public function rename($from, $to) {
         if ($from === null)
-            throw new Exception(get_class($this) . "::rename from key cannot be null");
+            throw new Exception(get_class($this) . '::rename from key cannot be null');
         elseif ($to === null)
-            throw new Exception(get_class($this) . "::rename to key cannot be null");
-        $this->doRequest(MogileFS::RENAME, Array('from_key' => $from, 'to_key' => $to));
+            throw new Exception(get_class($this) . '::rename to key cannot be null');
+        $this->doRequest(self::RENAME, Array('from_key' => $from, 'to_key' => $to));
         return true;
     }
 
     // Rename a file
     public function listKeys($prefix = null, $lastKey = null, $limit = null) {
         try {
-            return $this->doRequest(MogileFS::LIST_KEYS, Array('prefix' => $prefix, 'after' => $lastKey, 'limit' => $limit));
+            return $this->doRequest(self::LIST_KEYS, Array(
+                'prefix' => $prefix,
+                'after' => $lastKey,
+                'limit' => $limit
+            ));
         } catch (Exception $e) {
             if (strstr($e->getMessage(), 'ERR none_match'))
                 return Array();
@@ -272,13 +276,13 @@ class MogileFS {
     // Get a file from mogstored and return it as a string
     public function get($key) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::get key cannot be null");
+            throw new Exception(get_class($this) . '::get key cannot be null');
         $paths = $this->getPaths($key);
         foreach ($paths as $path) {
             $contents = '';
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_VERBOSE, ($this->debug > 0 ? 1 : 0));
-            curl_setopt($ch, CURLOPT_TIMEOUT, $this->getTimeout);
+            curl_setopt($ch, CURLOPT_VERBOSE, $this->_debug);
+            curl_setopt($ch, CURLOPT_TIMEOUT, $this->_getTimeout);
             curl_setopt($ch, CURLOPT_URL, $path);
             curl_setopt($ch, CURLOPT_FAILONERROR, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -295,13 +299,13 @@ class MogileFS {
     // Get a file from mogstored and send it directly to stdout by way of fpassthru()
     function getPassthru($key) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::getPassthru key cannot be null");
+            throw new Exception(get_class($this) . '::getPassthru key cannot be null');
         $paths = $this->getPaths($key);
         foreach ($paths as $path) {
             $fh = fopen($path, 'r');
             if ($fh) {
                 if (fpassthru($fh) === false)
-                    throw new Exception(get_class($this) . "::getPassthru failed");
+                    throw new Exception(get_class($this) . '::getPassthru failed');
                 fclose($fh);
             }
             return $success;
@@ -312,9 +316,9 @@ class MogileFS {
     // Save a file to the MogileFS
     public function setResource($key, $fh, $length) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::setResource key cannot be null");
+            throw new Exception(get_class($this) . '::setResource key cannot be null');
 
-        $location = $this->doRequest(MogileFS::CREATE_OPEN, Array('key' => $key));
+        $location = $this->doRequest(self::CREATE_OPEN, Array('key' => $key));
         $uri = $location['path'];
         $parts = parse_url($uri);
         $host = $parts['host'];
@@ -322,10 +326,10 @@ class MogileFS {
         $path = $parts['path'];
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_VERBOSE, ($this->debug > 0 ? 1 : 0));
+        curl_setopt($ch, CURLOPT_VERBOSE, $this->_debug);
         curl_setopt($ch, CURLOPT_INFILE, $fh);
         curl_setopt($ch, CURLOPT_INFILESIZE, $length);
-        curl_setopt($ch, CURLOPT_TIMEOUT, $this->putTimeout);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->_putTimeout);
         curl_setopt($ch, CURLOPT_PUT, true);
         curl_setopt($ch, CURLOPT_URL, $uri);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -335,10 +339,11 @@ class MogileFS {
         if ($response === false) {
             $error = curl_error($ch);
             curl_close($ch);
-            throw new Exception(get_class($this) . "::set $error");
+            throw new Exception(get_class($this) . "::set {$error}");
         }
         curl_close($ch);
-        $this->doRequest(MogileFS::CREATE_CLOSE, Array('key' => $key,
+        $this->doRequest(self::CREATE_CLOSE, Array(
+            'key' => $key,
             'devid' => $location['devid'],
             'fid' => $location['fid'],
             'path' => urldecode($uri)
@@ -349,10 +354,10 @@ class MogileFS {
 
     public function set($key, $value) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::set key cannot be null");
+            throw new Exception(get_class($this) . '::set key cannot be null');
         $fh = fopen('php://memory', 'rw');
         if ($fh === false)
-            throw new Exception(get_class($this) . "::set failed to open memory stream");
+            throw new Exception(get_class($this) . '::set failed to open memory stream');
         fwrite($fh, $value);
         rewind($fh);
         return $this->setResource($key, $fh, strlen($value));
@@ -360,7 +365,7 @@ class MogileFS {
 
     public function setFile($key, $filename) {
         if ($key === null)
-            throw new Exception(get_class($this) . "::setFile key cannot be null");
+            throw new Exception(get_class($this) . '::setFile key cannot be null');
         $fh = fopen($filename, 'r');
         if ($fh === false)
             throw new Exception(get_class($this) . "::setFile failed to open path {$filename}");
